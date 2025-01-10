07944612c0920a7c26972e66ee93995baa9b91e7
Malik Kemp <malikkemp@gmail.com>
2025-01-09 21:19:52 -0500

    Updated intro text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-5 text-secondary text-[17px] max-w-3xl leading-[25px]'
       >
-        I'm a new developer currently working as a 
-        Information Technology Specialist at a large hospital and in college working 
+        I'm a developer currently working as a 
+        Information Technology Specialist at a large hospital while in college working 
         to achieve my Bachelors of Science in Computer Science. Computers 
-        have always interested me and I enjoy being able to learn new technologies
-        and build my skillset. Programming and building small projects is what 
+        have always interested me and I enjoy being to learn new technologies
+        while building my skillset. Programming and building small projects is what 
         sparked my interest in computers. When I was 17, I made my first very small
         program in C# and Unity and haven't looked back. 
       </motion.p>
